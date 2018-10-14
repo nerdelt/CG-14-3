e3bbd56cba68e63591e5fcb6be0d581e54ca8653
nerdelt <nerdelt@gmail.com>
2018-10-13 21:16:59 -0500

    Timer First Attempt

---
+++ Timer/Timer/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace UserTimer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program allows you to set a timer in hours, minutes, seconds, or all three.");
+            Console.WriteLine("\nThe format of the timer is HH:MM:SS." +
+                "\n\nFor instance, a timer set for one hour and 15 minutes looks like this: 01:15:00 or 1:15");
+            Console.WriteLine("\nA timer for 15 seconds can be set like this: 00:00:15, or 0:0:15");
+            Console.WriteLine("\nIf you type in only a whole number, that is the number of HOURS the timer will be set for.");
+            Console.Write("\nPlease set your timer: ");
+
+            string inputTime = Console.ReadLine();
+
+            TimerSet timer = new TimerSet();
+
+            try
+            {
+                timer.SetTimer(inputTime);
+                Console.Write("\nTo start timer, press ENTER.");
+                
+            }
+
+            catch(Exception ex)
+            {
+                Console.WriteLine($"\nSorry, we could not start your timer.");
+            }
+
+            string start = Console.ReadLine();
+            timer.StartTimer(start);   
+
+            Console.ReadLine();
+
+
+        }
+       
+    }
+}
---
+++ Timer/Timer/TimerSet.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Timers;
+
+namespace UserTimer
+{
+    public class TimerSet
+    {
+        public TimeSpan ParsedTime { get; set; }
+        public Timer UserTimer { get; set; }
+        public Double TimerInput { get; set; }
+
+        public void SetTimer(string inputTime)
+        {
+            ParsedTime = TimeSpan.Parse(inputTime);
+
+            TimeSpan duration = ParsedTime.Duration();
+
+            Console.WriteLine($"\nYour timer is set for {duration}");
+        }
+
+        public void StartTimer(string start)
+        { 
+
+            TimerInput = ParsedTime.TotalMilliseconds;
+
+            UserTimer = new Timer(TimerInput);
+
+            UserTimer.Start();
+
+            UserTimer.Elapsed += UserTimer_Elapsed;
+         
+         }
+
+        private void UserTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            UserTimer.Stop();
+            Console.WriteLine("\nDing! Your timer has ended.");
+        }
+    }
+}

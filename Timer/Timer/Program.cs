c5114a98275fb4e7145057c8ff5eb31baa02d6e5
nerdelt <41270734+nerdelt@users.noreply.github.com>
2018-10-14 13:09:44 -0500

    Update Program.cs

--- Timer/Timer/Program.cs
+++ Timer/Timer/Program.cs
@@ -16,7 +16,7 @@ namespace UserTimer
             Console.WriteLine("This program allows you to set a timer in hours, minutes, seconds, or all three.");
             Console.WriteLine("\nThe format of the timer is HH:MM:SS." +
                 "\n\nFor instance, a timer set for one hour and 15 minutes looks like this: 01:15:00 or 1:15");
-            Console.WriteLine("\nA timer for 15 seconds can be set like this: 00:00:15, or 0:0:15");
+            Console.WriteLine("\nA timer for 15 seconds can be set like this: 00:00:15 or 0:0:15");
             Console.WriteLine("\nIf you type in only a whole number, that is the number of HOURS the timer will be set for.");
             Console.Write("\nPlease set your timer: ");
 
